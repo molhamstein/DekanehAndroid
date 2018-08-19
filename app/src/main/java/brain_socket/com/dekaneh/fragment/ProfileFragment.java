415e14c7f26e745e964e20a8333457190c55a077
Ayham Orfali <orfali.ayham@gmail.com>
2018-08-19 05:04:40 +0300

    added profile edit bottom sheet

--- app/src/main/java/brain_socket/com/dekaneh/fragment/ProfileFragment.java
+++ app/src/main/java/brain_socket/com/dekaneh/fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package brain_socket.com.dekaneh.fragment;
 
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -8,11 +9,16 @@ import brain_socket.com.dekaneh.R;
 import brain_socket.com.dekaneh.adapter.OrdersAdapter;
 import brain_socket.com.dekaneh.base.BaseFragment;
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class ProfileFragment extends BaseFragment {
 
     @BindView(R.id.profileOrdersRV)
     RecyclerView profileOrdersRV;
+    @BindView(R.id.bottomSheet)
+    View bottomSheet;
+
+    BottomSheetBehavior behavior;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -28,6 +34,9 @@ public class ProfileFragment extends BaseFragment {
 
         profileOrdersRV.setLayoutManager(new LinearLayoutManager(getContext()));
         profileOrdersRV.setAdapter(new OrdersAdapter());
+        behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
 
     }
 
@@ -39,5 +48,14 @@ public class ProfileFragment extends BaseFragment {
     @Override
     public String TAG() {
         return this.getClass().getSimpleName();
+    }
+
+    @OnClick(R.id.profileSettingsIcon)
+    public void onSettingsActionClick(){
+    }
+
+    @OnClick(R.id.profileEditIcon)
+    public void onEditActionClick(){
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
     }
 }
