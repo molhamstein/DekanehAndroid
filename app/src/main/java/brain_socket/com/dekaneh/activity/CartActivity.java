32be89866e3708d5fa96c9cc85f97abf640ef4ac
Ayham Orfali <orfali.ayham@gmail.com>
2018-08-28 12:01:41 +0300

    minor tweaks

--- app/src/main/java/brain_socket/com/dekaneh/activity/CartActivity.java
+++ app/src/main/java/brain_socket/com/dekaneh/activity/CartActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import brain_socket.com.dekaneh.R;
 import brain_socket.com.dekaneh.adapter.CartOrdersAdapter;
@@ -16,6 +18,8 @@ public class CartActivity extends BaseActivity {
 
     @BindView(R.id.cartOrderRV)
     RecyclerView ordersRV;
+    @BindView(R.id.cartToolbar)
+    Toolbar toolbar;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, CartActivity.class);
@@ -27,8 +31,24 @@ public class CartActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         ordersRV.setLayoutManager(new LinearLayoutManager(this));
         ordersRV.setAdapter(new CartOrdersAdapter());
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return false;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
